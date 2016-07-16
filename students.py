039b31e53ba4635419c26fb7ea47da8ca9d65257
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-07-16 13:31:35 +0300

    Updated methods for getting the last name

--- students.py
+++ students.py
@@ -4,10 +4,12 @@ student = {'Name_Details': ['Kimani', 'Ndegwa'],
            'Year of Birth': datetime(1994, 5, 15),
            'Phone Number': '0726114094'}
 
-'''Print Last Name Only'''
+'''Print Last Name Only: First Method'''
 names = student['Name_Details']
 print ("Last Name: " + names[1])
-print(student.get('Name_Details.get(1)'))
+'''Print Last Name Only: Second Method'''
+print ("Last Name: " + student['Name_Details'][1])
+'''Remaining Exercises'''
 print('My names are ' + str(student['Name_Details']))
 print(student['Year of Birth'])
 print(student['Phone Number'])
